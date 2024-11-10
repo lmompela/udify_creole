4ecfe9da7635144a2fb4659f77128d3d7cc90bef
lmompela <lmompela@iu.edu>
2024-11-10 13:25:10 -0500

    Update download_ud_data.sh

--- scripts/download_ud_data.sh
+++ scripts/download_ud_data.sh
@@ -3,15 +3,16 @@
 # Can download UD 2.3 or 2.4
 UD_2_3="https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-2895/ud-treebanks-v2.3.tgz?sequence=1&isAllowed=y"
 UD_2_4="https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-2988/ud-treebanks-v2.4.tgz?sequence=4&isAllowed=y"
+UD_2_14="https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-5502/ud-treebanks-v2.14.tgz?sequence=1&isAllowed=y"
 
-DATASET_DIR="data/ud-treebanks-v2.3"
+DATASET_DIR="data/ud-treebanks-v2.14"
 
 ARCHIVE="ud_data.tgz"
 
 
 echo "Downloading UD data..."
 
-curl ${UD_2_3} -o ${ARCHIVE}
+curl ${UD_2_14} -o ${ARCHIVE}
 
 tar -xvzf ${ARCHIVE} -C ./data
 mv ${ARCHIVE} ./data
